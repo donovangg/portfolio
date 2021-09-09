e2823b2259efaa42d4824e645054172fc134ad69
Donovan Gomez <donovanjuniogomez@gmail.com>
2021-09-09 02:09:26 -0700

    add Layout component

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 export default function portfolio() {
     return (
-        <div>
+        <Layout>
             portfolio
-        </div>
+        </Layout>
     )
 }
