b1cd1e97e43f3bd7ced57fc51ca6f68270a1360d
Donovan Gomez <donovanjuniogomez@gmail.com>
2021-09-14 23:54:39 -0700

    display project data

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,46 +2,66 @@ import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { Flex, Text, Box } from "@chakra-ui/react"
 
 export default function Projects({ data }) {
   console.log(data)
- 
+
   const projects = data.allMarkdownRemark.nodes
   return (
     <Layout>
-      <div>
+      <Flex
+        border="2px"
+        margin="auto"
+        justifyContent="space-around"
+        width="90%"
+        borderColor="red.400"
+      >
         {projects.map(project => (
-          <Link to={"/projects" + project.frontmatter.slug} key={project.id}>
-            <div>
-              <h3>{project.frontmatter.title}</h3>
-              <p>{project.frontmatter.stack}</p>
-              <GatsbyImage image={getImage(project.frontmatter.thumb)}  />
-              {project.frontmatter.date} 
-            </div>
-          </Link>
+          <Box
+            border="2px"
+            width="22rem"
+            borderColor="blue.400"
+            justifyContent="center"
+          >
+            <Link to={"/projects" + project.frontmatter.slug} key={project.id}>
+              <Flex flexDir="column" border="2px" height="100%" width="100%">
+                <Box flex="1">
+                  <h3>{project.frontmatter.title}</h3>
+                  <p>{project.frontmatter.stack}</p>
+                </Box>
+                <Box flex="1">
+                  <GatsbyImage
+                    image={getImage(project.frontmatter.thumb)}
+                    width="100%"
+                    height="100%"
+                  />
+                </Box>
+              </Flex>
+            </Link>
+          </Box>
         ))}
-      </div>
+      </Flex>
     </Layout>
   )
 }
 
 // export page query
 export const query = graphql`
-query ProjectPage {
-  allMarkdownRemark {
-    nodes {
-      frontmatter {
-        title
-        stack
-        slug
-        thumb {
-          childImageSharp {
-            gatsbyImageData(layout: FULL_WIDTH)
+  query MyQuery {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          title
+          stack
+          slug
+          thumb {
+            childImageSharp {
+              gatsbyImageData(layout: FULL_WIDTH)
+            }
           }
         }
       }
     }
   }
-}
-
 `
