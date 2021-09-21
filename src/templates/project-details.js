3d55f671b7200e0992463da1994ce6b9f3039a87
Donovan Gomez <donovanjuniogomez@gmail.com>
2021-09-20 23:08:01 -0700

    add query for each generated page

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,16 +1,37 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import { StaticImage } from 'gatsby-plugin-image'
+import { StaticImage, GatsbyImage, getImage } from 'gatsby-plugin-image'
+import { graphql } from 'gatsby'
 
-export default function ProjectDetails() {
+export default function ProjectDetails({ data }) {
+    const { html } = data.markdownRemark
+    const { title, stack, thumb } = data.markdownRemark.frontmatter
     return (
         <Layout>
             <div>
                 <h2>Title</h2>
                 <h4>Stack</h4>
-                {/* <StaticImage /> */}
+                <GatsbyImage image={getImage(data.markdownRemark.frontmatter.thumb.childImageSharp.gatsbyImageData)} />
             </div>
-            {/* <div dangerouslySetInnerHTML={} /> */}
+            <div dangerouslySetInnerHTML={{__html: html}} />
         </Layout>
     )
 }
+
+export const query = graphql`
+query ProjectDetails($slug: String) {
+    markdownRemark(frontmatter: {slug: {eq: $slug}}) {
+      html
+      frontmatter {
+        stack
+        title
+        thumb {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+      }
+    }
+  }
+  
+`;
