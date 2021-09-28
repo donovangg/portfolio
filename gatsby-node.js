0e28edd7cda1482969e1a8c34d3701bfbcea6177
Donovan Gomez <donovanjuniogomez@gmail.com>
2021-09-28 00:44:26 -0700

    add multiple queries for blog page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 exports.createPages = async ({ graphql, actions}) => {
-  const { projects } = await graphql(`
+  const { createPage } = actions;
+  const projects  = await graphql(`
   query Projects {
     allMarkdownRemark(filter: {frontmatter: {category: {eq: "project"}}}) {
       nodes {
@@ -11,11 +12,18 @@ exports.createPages = async ({ graphql, actions}) => {
       }
     }
   }
-  
-  `)
-  
+  `).then(result => {
+    result.data.allFile.edges.forEach(({ node }) => {
+      createPage({
+        path: '/projects/' + node.frontmatter.slug,
+        component: path.resolve('./src/templates/project-details.js'),
+        context: { slug: node.frontmatter.slug }
+    });
+    });
+  })  
+
   // create another query for the blog page
-  const { blogs } = await graphql(`
+  const  blogs  = await graphql(`
   query Blog {
     allMarkdownRemark(filter: {frontmatter: {category: {eq: "blog"}}}) {
       nodes {
@@ -25,22 +33,29 @@ exports.createPages = async ({ graphql, actions}) => {
       }
     }
   }
-  `)
-
-  projects.data.allMarkdownRemark.nodes.forEach(node => {
-      actions.createPage({
-          path: '/projects/' + node.frontmatter.slug,
-          component: path.resolve('./src/templates/project-details.js'),
-          context: { slug: node.frontmatter.slug }
-      })
-  })
-
-   // generate page for blog page
-   blogs.data.allMarkdownRemark.nodes.forEach(node => {
-    actions.createPage({
-      path: '/blog/' + node.frontmatter.slug,
-      component: path.resolve('./src/templates/blog-details.js'),
-      context: { slug: node.frontmatter.slug }
-  })
-   })
+  `).then(result => {
+    result.data.allFile.edges.forEach(({ node }) => {
+      createPage({
+        path: '/blog/' + node.frontmatter.slug,
+        component: path.resolve('./src/templates/blog-details.js'),
+        context: { slug: node.frontmatter.slug }
+    });
+    });
+  });
 }
+//   projects.data.allMarkdownRemark.nodes.forEach(node => {
+//       actions.createPage({
+//           path: '/projects/' + node.frontmatter.slug,
+//           component: path.resolve('./src/templates/project-details.js'),
+//           context: { slug: node.frontmatter.slug }
+//       })
+//   })
+
+//    blogs.data.allMarkdownRemark.nodes.forEach(node => {
+//     actions.createPage({
+//       path: '/blog/' + node.frontmatter.slug,
+//       component: path.resolve('./src/templates/blog-details.js'),
+//       context: { slug: node.frontmatter.slug }
+//   })
+//    })
+// }
