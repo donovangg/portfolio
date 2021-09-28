3f11bd070c987059453939fc729c1be4dea81f36
Donovan Gomez <donovanjuniogomez@gmail.com>
2021-09-27 17:51:00 -0700

    Add react helmet to layout

--- src/blogs/first-blog.md
+++
@@ -1,8 +0,0 @@
----
-title: Hello world
-slug: hello-world
-category: blog
----
-
-# yeet
-First blog hi.
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,16 @@
 import React from "react"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
+import { Helmet } from "react-helmet"
 
 export default function Layout({ children }) {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content="Web developer based in California." />
+        <title>Donovan Gomez | Web developer</title>
+      </Helmet>
       <Navbar />
       <div>{children}</div>
     </div>
